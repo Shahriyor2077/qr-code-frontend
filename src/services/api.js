2ccc8205e0cd289a3f4ddc1fad42e2cd751c03d7
shookh_uz <zshahriyor40@gmail.com>
2025-12-18 00:00:52 +0500

    Add backend API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: import.meta.env.PROD 
+    ? 'https://qr-code-backend-and-bot-3.onrender.com' 
+    : '',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
